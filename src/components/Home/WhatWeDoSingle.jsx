1fd5d7bee5828ad983a10e2a5d7fb0c289accfd9
Kawsar Ahmed <coder.kawsar@gmail.com>
2024-05-21 19:25:02 +0600

    shadow fix on what we do section

--- src/components/Home/WhatWeDoSingle.jsx
+++ src/components/Home/WhatWeDoSingle.jsx
@@ -5,9 +5,9 @@ function WhatWeDoSingle({ title, description, isCenter }) {
     <div
       className={`${
         isCenter
-          ? "bg-white rounded-2xl shadow-lg"
+          ? "bg-[#fcf9f9] rounded-2xl shadow-2xl hover:bg-white"
           : "hover:shadow-xl bg-[#fff4f4]"
-      } w-52 p-6 cursor-pointer`}
+      } w-52 p-6 cursor-pointer shadow`}
     >
       <img src={imgClock} className="p-3 rounded-lg bg-[#f56767]" alt="" />
       <h3 className="mt-9 mb-[13px] text-[#FF3300] text-[25px] font-bold">
